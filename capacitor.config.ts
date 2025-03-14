b574b09a193d9ff949f4c7419ab27aa1fe7eb64a
supra1302 <davidcatalim@gmail.com>
2025-03-14 14:28:03 +0000

    fixed keyboard resizing bug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,22 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.konsta-sveltekit.app',
   appName: 'konsta-sveltekit-app',
   webDir: 'build',
   // uncomment this to enable hot reload
-  server: {
-		url: 'http://192.168.0.8:5173',
-		cleartext: true
-	}
+  // server: {
+	// 	url: 'http://192.168.0.8:5173',
+	// 	cleartext: true
+	// },
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true, // There is an Android bug that prevents the keyboard from resizing the WebView when the app is in full screen. This setting, if set to true, add a workaround that resizes the WebView even when the app is in full screen.
+    },
+  },
 };
 
 export default config;
